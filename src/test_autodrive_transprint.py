6d41b03ba7f679a3268e05756c731df87b2b10f4
工藤 綾子 (Ayako Kudoh) <kudoh.a@wingarc.com>
2024-01-12 17:19:43 +0900

    パラメータを変数化した

--- src/test_autodrive_transprint.py
+++ src/test_autodrive_transprint.py
@@ -6,9 +6,12 @@ from autodrive_transprint import TransPrint  # テスト対象のモジュール
 class TestTransPrint(unittest.TestCase):
 
     def test(self):
+        base_url = "https://qa224.dev.spa-cloud.com/spa/"
+        user_id = "spa"
+        password = "spa"
         transprint = TransPrint()
-        transprint.start_browser("https://qa224.dev.spa-cloud.com/spa/")
-        transprint.login("spa","spa")
+        transprint.start_browser(base_url)
+        transprint.login(user_id,password)
         transprint.open_cloud_settings()
         transprint.select_delivery_setting()
         time.sleep(5)
